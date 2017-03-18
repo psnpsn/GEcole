25153c53517227454916d52b2eb27c9d05c61e9e
Houssem Guesmi <7oucem-guesmi@users.noreply.github.com>
2017-03-18 01:35:34 +0100

    code pour retour au menu precedant

--- src/GUI/centre/GestionEleveController.java
+++ src/GUI/centre/GestionEleveController.java
@@ -5,11 +5,19 @@
  */
 package GUI.centre;
 
+import GUI.LoginController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import main_pack.Main_class;
 
 /**
  * FXML Controller class
@@ -24,10 +32,19 @@ public class GestionEleveController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void click_ajouter(ActionEvent event) {
+        try {
+            URL loader = getClass().getResource("ajoutEleve.fxml");
+            AnchorPane middle = FXMLLoader.load(loader);
+
+            BorderPane border = Main_class.getRoot();
+            border.setCenter(middle);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @FXML
@@ -37,6 +54,18 @@ public class GestionEleveController implements Initializable {
     @FXML
     private void click_lister(ActionEvent event) {
     }
+    @FXML
+    private void click_retour(ActionEvent event) {
+        try {
+            URL loader = getClass().getResource("mainwindow.fxml");
+            AnchorPane middle = FXMLLoader.load(loader);
+
+            BorderPane border = Main_class.getRoot();
+            border.setCenter(middle);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     @FXML
     private void click_modifier(ActionEvent event) {
@@ -45,5 +74,5 @@ public class GestionEleveController implements Initializable {
     @FXML
     private void click_supprimer(ActionEvent event) {
     }
-    
+
 }
